9c03f492d7bc5fdafa6a2ad8e61ce234c64f8ab6
rivrkitty <rivrkitty@protonmail.com>
2021-11-08 14:10:28 +0100

    Removed commented code

--- contracts/RivrKitty.sol
+++ contracts/RivrKitty.sol
@@ -14,52 +14,80 @@ import "./interfaces/IUniswapV2Router02.sol";
 contract RivrKitty is ERC20, Ownable {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
-    
+
     address public immutable firstTimelockAddress;
     address public immutable secondTimelockAddress;
     address public immutable thirdTimelockAddress;
-    
+
     uint256 public immutable startTime;
-    
+
     bool public whaleBlockingEnabled = false;
-    
+
     uint256 private constant TOKENS_COUNT = 1000000000000;
     uint256 private constant TEAM_TOKENS_PERCENT = 4;
-    
+
     uint256 public constant TEAM_TOKENS_PART_LOCK = 4 weeks;
     uint256 public constant WHALES_TIMEOUT = 1 weeks;
 
-    constructor(address _devAddress) ERC20("RivrKitty", "KITTY") payable {
+    constructor(address _devAddress) payable ERC20("RivrKitty", "KITTY") {
         startTime = block.timestamp;
-        
-        uint256 allTokens = TOKENS_COUNT * 10 ** decimals();
+
+        uint256 allTokens = TOKENS_COUNT * 10**decimals();
 
         uint256 teamAmount = allTokens.mul(TEAM_TOKENS_PERCENT).div(100);
         uint256 teamPartAmount = teamAmount.div(4);
-        
+
         ERC20._mint(_devAddress, teamPartAmount);
-        firstTimelockAddress = _mintTokens(block.timestamp + TEAM_TOKENS_PART_LOCK, teamPartAmount, _devAddress);
-        secondTimelockAddress = _mintTokens(block.timestamp + TEAM_TOKENS_PART_LOCK * 2, teamPartAmount, _devAddress);
-        thirdTimelockAddress = _mintTokens(block.timestamp + TEAM_TOKENS_PART_LOCK * 3, teamPartAmount, _devAddress);
-        
+        firstTimelockAddress = _mintTokens(
+            block.timestamp + TEAM_TOKENS_PART_LOCK,
+            teamPartAmount,
+            _devAddress
+        );
+        secondTimelockAddress = _mintTokens(
+            block.timestamp + TEAM_TOKENS_PART_LOCK * 2,
+            teamPartAmount,
+            _devAddress
+        );
+        thirdTimelockAddress = _mintTokens(
+            block.timestamp + TEAM_TOKENS_PART_LOCK * 3,
+            teamPartAmount,
+            _devAddress
+        );
+
         ERC20._mint(address(_msgSender()), allTokens.sub(teamAmount));
     }
-    
-    function _mintTokens(uint256 _releaseTime, uint256 _amount, address _devAddress) internal returns (address) {
-        TokenTimelock firstTimelock = new TokenTimelock(this, _devAddress, _releaseTime);
+
+    function _mintTokens(
+        uint256 _releaseTime,
+        uint256 _amount,
+        address _devAddress
+    ) internal returns (address) {
+        TokenTimelock firstTimelock = new TokenTimelock(
+            this,
+            _devAddress,
+            _releaseTime
+        );
         ERC20._mint(address(firstTimelock), _amount);
         return address(firstTimelock);
     }
-    
-    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        // if (whaleBlockingEnabled && (block.timestamp - startTime) < WHALES_TIMEOUT) {
-        //     uint256 newBal = this.balanceOf(recipient).add(amount);
-        //     uint256 maxBal = totalSupply().mul(3).div(100);
-        //     require(newBal < maxBal, "!whale reject");
-        // }
-        
+
+    function transfer(address recipient, uint256 amount)
+        public
+        virtual
+        override
+        returns (bool)
+    {
+        if (
+            whaleBlockingEnabled &&
+            (block.timestamp - startTime) < WHALES_TIMEOUT
+        ) {
+            uint256 newBal = this.balanceOf(recipient).add(amount);
+            uint256 maxBal = totalSupply().mul(3).div(100);
+            require(newBal < maxBal, "!whale reject");
+        }
+
         _transfer(_msgSender(), recipient, amount);
-        
+
         return true;
     }
 
