5f27889e0de8c850b7e785f2c6c9aceaebccc005
rivrkitty <rivrkitty@protonmail.com>
2021-11-17 17:14:51 +0100

    Added deployPool script

--- scripts/deployPool.js
+++ scripts/deployPool.js
@@ -4,23 +4,15 @@ const ethers = hardhat.ethers;
 
 // moonbaseAlpha
 const chefAddress = "0x364de9a286bEcd1848Ee7D670431eb0fEC0596D3";
-const huckleChef = "0xd45F54838dbfCe0F976bD430bE5f8678479E1510";
-const finnAddress = "0x31b1644f8379a22d25f845a67f1ab346e76001aa";
 
 // TODO: uncomment when releasing
 // const multisigAddress = "0xF21A3A541C3c96100A7BFb9b249220cdB631907d";
-
-const addRewardChefAddress = huckleChef;
-const addRewardAddress = finnAddress;
 
 const deployArgs = {
   allocPoint: 100,
   lpToken: "0x7d92ac7065371e0e4a201eae7a263cd8146d7ccc",
   depositFeeBP: 0,
   withUpdate: false,
-  addRewardChef: addRewardChefAddress,
-  addRewardChefPid: 7,
-  addRewardToken: addRewardAddress,
 };
 
 const poolInfo = {
@@ -29,11 +21,7 @@ const poolInfo = {
   chefAddress: chefAddress,
   tokenAddress: deployArgs["lpToken"],
   buyTokenUrl: "https://www.huckleberry.finance/",
-  rewardTokensDecimals: [18, 18],
-  addRewardChefAddress: deployArgs["addRewardChef"],
-  addRewardChefPid: deployArgs["addRewardChefPid"],
-  addRewardChefPerBlockName: "finnPerSecond",
-  addRewardChefPerBlockType: "seconds",
+  rewardTokensDecimals: [18],
 };
 
 async function getTokenSymbol(tokenAddress, signer) {
@@ -66,15 +54,12 @@ async function updatePoolInfo(chef, signer) {
   poolInfo["tokenAssetAddresses"] = [token0Address, token0Address];
   poolInfo["id"] = `${
     poolInfo["platform".toLowerCase()]
-  }-${token0Symbol.toLowerCase()}-${token1Symbol.toLowerCase()}-multi`;
+  }-${token0Symbol.toLowerCase()}-${token1Symbol.toLowerCase()}`;
   poolInfo["name"] = `${token0Symbol}-${token1Symbol}`;
   poolInfo["buyTokenUrl"] += `#/add/${token0Address}/${token1Address}`;
   const rewardAddress = await chef.paws();
-  poolInfo["rewardTokens"] = [
-    await getTokenSymbol(rewardAddress, signer),
-    await getTokenSymbol(addRewardAddress, signer),
-  ];
-  poolInfo["rewardTokensAddress"] = [rewardAddress, addRewardAddress];
+  poolInfo["rewardTokens"] = [await getTokenSymbol(rewardAddress, signer)];
+  poolInfo["rewardTokensAddress"] = [rewardAddress];
 }
 
 async function main() {
@@ -92,7 +77,7 @@ async function main() {
   console.log("Deploying...", signer.address);
 
   console.log("deployArgs", deployArgs);
-  await chef.addWithAddReward(...Object.values(deployArgs));
+  await chef.add(...Object.values(deployArgs));
 
   // Update this before adding
   console.log("Created pool", poolInfo);
